fa1d88efc19dd4f246a79b4dc740879eaad9b41a
AteyabaSan <remikabbs@gmail.com>
2024-09-03 11:11:52 +0200

    Initial commit

---
+++ src/main/java/com/joparis2024/J0Paris2024Application.java
@@ -0,0 +1,13 @@
+package com.joparis2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J0Paris2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J0Paris2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joparis2024/J0Paris2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joparis2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J0Paris2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
