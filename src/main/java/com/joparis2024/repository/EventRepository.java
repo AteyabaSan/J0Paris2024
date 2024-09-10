1cb1eca60ec262cc2e29b26a2985bb531f12b245
AteyabaSan <remikabbs@gmail.com>
2024-09-10 13:54:01 +0200

    Nettoyage de la branche des fichiers non pertinents pour les tests unitaires

--- src/main/java/com/joparis2024/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.joparis2024.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.joparis2024.model.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- src/main/java/com/joparis2024/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joparis2024.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.joparis2024.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
