8141ff17db7400d01f04a30897a9072a9e4e2c72
AteyabaSan <remikabbs@gmail.com>
2024-09-16 20:13:42 +0200

    Restauration des fichiers repository supprimés lors du nettoyage

---
+++ src/main/java/com/joparis2024/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.joparis2024.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.joparis2024.model.Order;
+import com.joparis2024.model.User;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUser(User user);
+}
---
+++ src/main/java/com/joparis2024/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.joparis2024.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.joparis2024.model.Payment;
+import com.joparis2024.model.Order;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Payment findByOrder(Order order);
+}
